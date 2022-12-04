5c19debcd1773e77d879307fbd9ba5ef1bb50483
Thermiee <navyblack16@yahoo.com>
2022-12-04 01:24:16 +0100

    consume api

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import styles from '../styles/Home.module.css';
+import React, { useState, useEffect } from 'react';
+
+export default function Home() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@nutanbhogendrasharma/")
+      .then(res => res.json())
+      .then((data) => {
+        setIsLoaded(true); setBlogs(data.items);
+      }, (error) => { setIsLoaded(true); setError(error); })
+  }, [])
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (<div className={styles.container}>
+      {blogs.map(item => (<div className={styles.main}> <div> <img src={item.thumbnail}
+        width='150px' height='150px' /> </div> <div> <a href={item.link}
+          target="_blank">{item.title}</a> </div> </div>))} </div>);
+  }
+}
