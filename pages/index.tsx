8023d19a877cb24721b7a2edcc41185cdfe70d03
Thermiee <navyblack16@yahoo.com>
2022-12-04 01:27:09 +0100

    add feed title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,15 @@ import React, { useState, useEffect } from 'react';
 export default function Home() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [feed, setFeed] = useState([]);
   const [blogs, setBlogs] = useState([]);
   useEffect(() => {
     fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@nutanbhogendrasharma/")
       .then(res => res.json())
       .then((data) => {
-        setIsLoaded(true); setBlogs(data.items);
+        setIsLoaded(true);
+        setFeed(data.feed);
+        setBlogs(data.items);
       }, (error) => { setIsLoaded(true); setError(error); })
   }, [])
   if (error) {
@@ -18,8 +21,15 @@ export default function Home() {
     return <div>Loading...</div>;
   } else {
     return (<div className={styles.container}>
-      {blogs.map(item => (<div className={styles.main}> <div> <img src={item.thumbnail}
-        width='150px' height='150px' /> </div> <div> <a href={item.link}
-          target="_blank">{item.title}</a> </div> </div>))} </div>);
+      <h1>{feed.title}</h1>
+      {blogs.map(item => (<div className={styles.main}>
+        <div> <img src={item.thumbnail}
+          width='150px' height='150px' />
+        </div>
+        <div> <a href={item.link}
+          target="_blank">{item.title}</a>
+        </div>
+      </div>))}
+    </div>);
   }
 }
